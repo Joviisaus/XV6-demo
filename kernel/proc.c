204f225d628bbf99a05b432d4bbadaa3641c05ac
OuyangJing <ouyangj999@gmail.com>
2023-03-29 15:45:54 +0800

    trace:

--- kernel/proc.c
+++ kernel/proc.c
@@ -295,6 +295,8 @@ fork(void)
     return -1;
   }
   np->sz = p->sz;
+  np->mask = p->mask;
+  np->parent = p;
 
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
--- kernel/syscall.c
+++ kernel/syscall.c
@@ -101,7 +101,7 @@ extern uint64 sys_unlink(void);
 extern uint64 sys_link(void);
 extern uint64 sys_mkdir(void);
 extern uint64 sys_close(void);
-extern uint64 sys_sysinfo(void);
+extern uint64 sys_trace(void);
 
 // An array mapping syscall numbers from syscall.h
 // to the function that handles the system call.
